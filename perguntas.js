287ccef6735659bd56503c4ac7f942d08ba5c934
Dudastar <00001111008930SP@al.educacao.sp.gov.br>
2024-10-27 01:09:59 -0300

    flashcards

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimento Geral',
+    'Quantas cores tem o arco-íris?',
+    '7 cores'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'De qual banda de rock que o cantor Freddie Mercury foi vocalista?',
+    'Queen'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Qual é o metal mais caro do mundo?',
+    'Ródio'
+)
+
+criaCartao(
+    'Conhecimento Geral',
+    'Quantas vogais há na palavra "pesqueiro"?',
+    ' Cinco vogais'
+)
